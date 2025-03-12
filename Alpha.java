1c4127e81995a918e75f8484b3b2b8af2a4b5f80
charan <charanbolapati@gmail.com>
2025-03-12 23:31:35 +0530

    Given Alphabet is Vowel or Consonant

---
+++ Alpha.java
@@ -0,0 +1,8 @@
+class Alpha
+{
+	public static void main(String args[])
+{
+char l='l';
+System.out.println(l=='a'||l=='e'||l=='i'||l=='o'||l=='u'||l=='A'||l=='E'||l=='I'||l=='O'||l=='U'? "vowel":"consonant");
+}
+}
